da306b16897c2370c5963134cae8708cb846408e
retroryan <ryan@grandcloud.com>
2014-09-17 13:53:56 -0500

    fixing webservice util trait.

--- app/actors/TweetAPI.scala
+++ app/actors/TweetAPI.scala
@@ -5,33 +5,24 @@ import play.api.libs.ws._
 import play.api.libs.ws.ning.{NingWSClient, NingAsyncHttpClientConfigBuilder}
 import play.api.libs.ws.DefaultWSClientConfig
 
-trait TweetAPI {
+object TweetAPI {
 
-    implicit val ec: ExecutionContext
-
-    lazy val builder = new NingAsyncHttpClientConfigBuilder(DefaultWSClientConfig())
-
-    class TweetWS extends WSAPI {
-        override def client: WSClient = new NingWSClient(builder.build())
-        override def url(url: String): WSRequestHolder = client.url(url)
-    }
-}
-
-object TweetAPI extends TweetAPI {
+    val builder = new NingAsyncHttpClientConfigBuilder(DefaultWSClientConfig())
+    val client: WSClient = new NingWSClient(builder.build())
 
     val TWEET_PROXY_URL = "http://search-twitter-proxy.herokuapp.com/search/tweets"
-
     val GEOCODE_URL = "http://maps.googleapis.com/maps/api/geocode/json"
 
-    override implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+    implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
 
     def tweetWS(query: String):Future[WSResponse] =
-        new TweetWS().url(TWEET_PROXY_URL).withQueryString("q" -> query).get()
+        client.url(TWEET_PROXY_URL).withQueryString("q" -> query).get()
 
     def geocodeWS(query: String):Future[WSResponse] =
-        new TweetWS().url(GEOCODE_URL).withQueryString(
+        client.url(GEOCODE_URL).withQueryString(
             "sensor" -> "false",
             "address" -> query)
             .get()
 
 }
+
